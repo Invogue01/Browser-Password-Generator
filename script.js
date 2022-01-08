0561a2df034f97eee3e335c2294da897e1ece7a4
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-08 20:54:21 +0000

    defined generatePassword function

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // 1. When button clicked present prompts to select criteria 
 // 2. Needs to prompt character length (min8 max128 characters), upperCase, specialSymbols, numbers
-// 3. 5 prompts in total
-// 4. Validate what user selected (input) if nothing selected make an alert informing that they MUST choose
+// 3. 4 prompts in total
+// 4. Validate wh// d. if ONLY password length selected return ALL LOWER CASE password usat user selected (input) if nothing selected make an alert informing that they MUST choose
 // 5. Ask to at least choose character length (how long is password)
 // 6. After password length selected and any other criteria is selected we need to validate (check what user entered/selected)
 // a. validate (check what user entered) and use what user selected to generate password
@@ -13,26 +11,37 @@ var generateBtn = document.querySelector("#generate");
 // a. i need to create var (or lego pieces) for each criteria as in UpperCase, Numbers, Symbols and so on.
 // b. use created var to randomly pick a character Math.floor (random)
 // c. add up (concat) all selected criteria to generate password.
-// d. if ONLY password length selected return ALL LOWER CASE password using ONLY lowerCase letters. Otherwise generate password according to user selected criteria.
+// ing ONLY lowerCase letters. Otherwise generate password according to user selected criteria.
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(params) {
+  console.log("hey you clicked a button")
+  return "Generated password will be here"
+}
+
+
 
 
 // Write password to the password input
 function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  let lengthChoice = window.prompt ("Please Choose Password length between 8 and 128 characters");
-  if (lengthChoice > 8 && lengthChoice < 128) 
-    alert ("Thank you");
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+  // let lengthChoice = window.prompt ("Please Choose Password length between 8 and 128 characters");
+  // if (lengthChoice > 8 && lengthChoice < 128) 
+  //   alert ("Thank you");
   
-  else (!lengthChoice)
-  alert ("you Must selected Password Length to continue")
-  return;
+  // else (!lengthChoice)
+  // alert ("you Must selected Password Length to continue")
+  // return;
   
  
 // let pickUpper= confirm ("Would you like to include UPPER cases?");
 
 
-//   passwordText.value = password;
+   passwordText.value = password;
 
 
 }
