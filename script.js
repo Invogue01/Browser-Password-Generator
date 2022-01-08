1c20b3e94a0303788f918e17fc76a04c8e974b7a
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-08 16:24:51 +0000

    getting window prompt to work

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// 1. When button clicked present prompts to select criteria 
+// 2. Needs to prompt character length (min8 max128 characters), upperCase, specialSymbols, numbers
+// 3. 5 prompts in total
+// 4. Validate what user selected (input) if nothing selected make an alert informing that they MUST choose
+// 5. Ask to at least choose character length (how long is password)
+// 6. After password length selected and any other criteria is selected we need to validate (check what user entered/selected)
+// a. validate (check what user entered) and use what user selected to generate password
 
-  passwordText.value = password;
+// 7. right a function that will use numbers, letters, symbols  and so on to do the Math and generate random password.
+// a. i need to create var (or lego pieces) for each criteria as in UpperCase, Numbers, Symbols and so on.
+// b. use created var to randomly pick a character Math.floor (random)
+// c. add up (concat) all selected criteria to generate password.
+// d. if ONLY password length selected return ALL LOWER CASE password using ONLY lowerCase letters. Otherwise generate password according to user selected criteria.
+
+
+// Write password to the password input
+function writePassword() {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  let lengthChoice = window.prompt ("Please Choose Password length between 8 and 128 characters");
+  if (lengthChoice > 8 && lengthChoice < 128) 
+    alert ("Thank you");
+  
+  else (!lengthChoice)
+  alert ("you Must selected Password Length to continue")
+  return;
+  
+ 
+// let pickUpper= confirm ("Would you like to include UPPER cases?");
+
+
+//   passwordText.value = password;
+
 
 }
-
-// Add event listener to generate button
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
